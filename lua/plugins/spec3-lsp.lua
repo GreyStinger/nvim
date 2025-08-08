a295f9c15423e5e8c23684638b4c75cc78d3324a
Jayden Andrews <jaydenandrews010@gmail.com>
2025-08-08 16:17:31 +0200

    updates

--- lua/plugins/spec3-lsp.lua
+++ lua/plugins/spec3-lsp.lua
@@ -54,7 +54,7 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
       for server, config in pairs(opts.servers) do
         config.capabilities = capabilities
-        -- vim.lsp.enable(server)
+        vim.lsp.enable(server)
         vim.lsp.config(server, config)
       end
     end,
